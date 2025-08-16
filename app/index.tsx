5486af6e46884ce109b198a86dd8c30d096ec19e
FinleyCox <111890705+FinleyCox@users.noreply.github.com>
2025-08-16 17:10:23 +0900

    test: カウンター作成

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { Component } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+class App extends Component {
+  state = {
+    count: 0,
+  }
+  onPress = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <View style={Styles.container}>
+        <Text>{this.state.count}</Text>
+        <Button title="Click me" onPress = {this.onPress} />
+      </View>
+    );
+  }
 }
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default App;
